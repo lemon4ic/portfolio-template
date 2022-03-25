9b49ead9d7b3473b9dcd153d672c2f6ebd4e2804
Леонид <leonid.kibirev@bk.ru>
2022-03-25 18:45:17 +0500

    Добавление динамического содержимого с помощью массивов

---
+++ data.php
@@ -0,0 +1,59 @@
+<?php
+
+return [
+    'about' => [
+        'name' => 'Леонид Кибирев',
+        'post' => 'web разработчик',
+        'email' => 'leonid.kibirev@bk.ru',
+        'phone' => '+79001002030',
+        'site' => 'google.com'
+    ],
+    'education' => [
+        [
+            'faculty' => 'Random faculty',
+            'univercity' => 'Smth Uivercity',
+            'yearStart' => 2015,
+            'yearEnd' => 2018
+        ],
+        [
+            'faculty' => 'Random faculty 2',
+            'univercity' => 'Smth Uivercity 2',
+            'yearStart' => 2010,
+            'yearEnd' => 2015
+        ]
+    ],
+    'languages' => [
+        'English', 'Russia', 'Spanish'
+    ],
+    'interests' => [
+        'увличение 1', 'Увличение 2'
+    ],
+    'aboutCareer' => 'lorem lorem',
+
+    'career' => [
+        [
+            'post' => 'post 1',
+            'plase' => 'City 1',
+            'duty' => 'lorem lorem 111',
+            'yearStart' => 2018,
+            'yearEnd' => 'настоящее время'
+            
+        ],
+        [
+            'post' => 'post 2',
+            'plase' => 'City 2',
+            'duty' => 'lorem lorem 222',
+            'yearStart' => 2010,
+            'yearEnd' => 2015
+        ],
+        [
+            'post' => 'post 3',
+            'plase' => 'City 3',
+            'duty' => 'lorem lorem 333',
+            'yearStart' => 2005,
+            'yearEnd' => 2009
+        ],
+    ]
+];
+
+?>
